4844384e88147b1c817cb1ff1c3d22f948e3fdbf
mauricio.minella@bravi.com.br <mauricio.minella@bravi.com.br>
2016-01-26 17:11:30 -0200

    First working version

--- tests/services/timesheetCalculationServiceTests.js
+++ tests/services/timesheetCalculationServiceTests.js
@@ -8,6 +8,16 @@ var Student = require('../src/services/timesheetCalculationService')
 
 describe('Calculation Service', function(){
   it('Rounded hours correctness using specific scenarion 1', function(){
+    var timeEntryList = [
+      {
+        description = "144449:6.4.0.4 - Set system context on login into Student Self-Management and Learner Portals",
+        start = new DateTime(2014, 1, 11),
+        duration = 20284,
+        isTimesheet = true
+      },
+      {
 
+      }
+    ];
   });
 });
